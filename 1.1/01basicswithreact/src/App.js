74314119a7b5b7719413e8aa6c0226835d6cdb05
Subodh Adhikari <adhikarisubodh77@gmail.com>
2024-01-05 16:56:59 +0545

    used react dom basics

---
+++ 1.1/01basicswithreact/src/App.js
@@ -0,0 +1,11 @@
+import New from './New';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <New /><span>from App</span>
+    </div>
+  );
+}
+
+export default App;
